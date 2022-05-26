7f6bed2c7ed7e7550607b5c6210dc7a2e47048b4
moisestech <m@moises.tech>
2022-05-26 09:31:08 -0400

    testing global vars

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Web3CryptoMX';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
