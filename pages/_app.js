19fa9726cd9f09d32e71be59fa9d08af4c3f86d1
moisestech <m@moises.tech>
2022-05-26 09:30:00 -0400

    test ga integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 
+// REACT | NEXT
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from '../lib/ga';
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <span className="theme-bejamas" />
