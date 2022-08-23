091e706d952366d6d82ad10ee7c3ce91ba11b234
moisestech <m@moises.tech>
2022-08-23 16:36:22 -0400

    adding GA tag in netlify variable

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import 'prismjs/themes/prism-tomorrow.css';
 // REACT | NEXT
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 import * as ga from '../lib/ga';
 
@@ -27,6 +28,19 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
+      
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
