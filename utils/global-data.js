e7af14cc0d5a23f5f2ad6e64f670b4ad0ffc7aae
moisestech <m@moises.tech>
2022-08-31 18:36:55 -0400

    update with slogan

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,10 +4,10 @@ export const getGlobalData = () => {
     : 'Web3CryptoMX';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'EL Nodo del Metaverso';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'EL Nodo del Metaverso';
 
   return {
     name,
